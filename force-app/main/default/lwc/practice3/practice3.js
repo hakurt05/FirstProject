7a1ebf59099de0bd1bf9719a508cd5e9d2d45f4f
hakurt05 <ekicihakan@hotmail.com>
2023-05-23 12:59:14 -0400

    data binding

---
+++ force-app/main/default/lwc/practice3/practice3.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class Practice3 extends LightningElement {
+
+message="Offer Received";
+
+handleChange(){
+
+    console.log('User typed something');
+}
+
+}
